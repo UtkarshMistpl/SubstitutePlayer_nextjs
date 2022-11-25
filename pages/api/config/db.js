58d80432548c7ef5990a80ba911024dfc12fa7ee
UtkarshMistpl <utkarsh@mistpl.com>
2022-11-25 16:32:11 +0530

    mongo uri env

--- pages/api/config/db.js
+++ pages/api/config/db.js
@@ -1,3 +1,6 @@
 import mongoose from "mongoose";
-const Client = async () => mongoose.connect(process.env.MONGO_URI);
+const Client = async () =>
+	mongoose.connect(
+		"mongodb+srv://utkarsh:hO3SZk1dKbzun0BS@cluster0.a4zxnps.mongodb.net/?retryWrites=true&w=majority"
+	);
 export default Client;
